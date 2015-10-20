eccfeb305d02e9646fea017b350fb3d07e9a2f67
Snjoo <samppa.hynninen@gmail.com>
2015-10-20 20:54:53 +0300

    React page for adding routines, not complete yet

--- src/main/resources/webapp/js/mainpage.js
+++ src/main/resources/webapp/js/mainpage.js
@@ -4,7 +4,7 @@ var MainPage = React.createClass({displayName: "MainPage",
     var title = '<h3>GymPal</h3>';
     return (
       React.createElement("div", {className: "row"}, 
-        React.createElement("div", {className: "large-3 medium-4 columns"}, 
+        React.createElement("div", {className: "large-2 medium-3 columns"}, 
           React.createElement("h5", null, "Navigation"), 
           React.createElement("nav", null, 
             React.createElement("ul", {className: "side-nav"}, 
@@ -12,13 +12,96 @@ var MainPage = React.createClass({displayName: "MainPage",
             )
           )
         ), 
-        React.createElement("div", {className: "large-9 medium-8 columns"}, 
-          React.createElement("h1", null, "GymPal - Your fitness companion!")
+        React.createElement("div", {className: "large-10 medium-9 columns"}, 
+          React.createElement("h1", null, "GymPal - Your fitness companion!"), 
+          React.createElement(RoutineForm, null)
         )
       )
     );
   }
 });
+
+var RoutineForm = React.createClass({displayName: "RoutineForm",
+  getInitialState: function() {
+    return {
+      name: "",
+      additionalInfo: "",
+      duration: "",
+      toughness: "",
+      exerciseList: ""
+    };
+  },
+
+  render: function() {
+    return(
+      React.createElement("form", null, 
+        React.createElement("div", {className: "row"}, 
+          React.createElement("div", {className: "large-12 medium-12 columns"}, 
+            React.createElement(RoutineInfo, {
+              nameChangeHandler: this.handleNameChange, 
+              additionalInfoChangeHandler: this.handleAdditionalInfoChange, 
+              durationChangeHandler: this.handleDurationChange, 
+              toughnessChangeHandler: this.handleToughnessChange}
+              ), 
+            React.createElement(Exercises, null)
+          )
+        )
+      )
+    );
+  },
+  handleNameChange: function(event) {
+    this.setState({name: event.target.value});
+  },
+  handleAdditionalInfoChange: function(event) {
+    this.setState({additionalInfo: event.target.value});
+  },
+  handleDurationChange: function(event) {
+    this.setState({duration: event.target.value});
+  },
+  handleToughnessChange: function(event) {
+    this.setState({toughness: event.target.value});
+  },
+});
+
+var RoutineInfo = React.createClass({displayName: "RoutineInfo",
+  render: function() {
+    return(
+      React.createElement("div", {className: "row"}, 
+        React.createElement("div", {className: "large-12 medium-12 columns"}, 
+          React.createElement("div", {className: "row"}, 
+            React.createElement("label", {htmlFor: "name"}, "Routine name"), " ", 
+            React.createElement("input", {id: "name", type: "text", onChange: this.props.nameChangeHandler})
+          ), 
+          React.createElement("div", {className: "row"}, 
+            React.createElement("label", {htmlFor: "toughness"}, "Toughness (0-100)"), " ", 
+            React.createElement("input", {id: "toughness", type: "number", min: "0", max: "100", onChange: this.props.toughnessChangeHandler})
+          ), 
+          React.createElement("div", {className: "row"}, 
+            React.createElement("label", {htmlFor: "duration"}, "Duration (minutes)"), " ", 
+            React.createElement("input", {id: "duration", type: "number", min: "0", max: "1000", onChange: this.props.durationChangeHandler})
+          ), 
+          React.createElement("div", {className: "row"}, 
+            React.createElement("label", {htmlFor: "additionalInfo"}, "Additional info"), " ", 
+            React.createElement("textarea", {id: "additionalInfo", placeholder: "More information about routine", rows: "4", cols: "80", onChange: this.props.additionalInfoChangeHandler})
+          )
+        )
+      )
+    );
+  }
+});
+
+var Exercises = React.createClass({displayName: "Exercises",
+  render: function() {
+    return(
+      React.createElement("div", {className: "row"}, 
+        React.createElement("div", {className: "large-12 medium-12 columns"}, 
+          React.createElement("h3", null, "Exercises")
+        )
+      )
+    );
+  }
+});
+
 ReactDOM.render(
   React.createElement(MainPage, null),
   document.getElementById('container')
--- src/main/resources/webapp/jsx/mainpage.js
+++ src/main/resources/webapp/jsx/mainpage.js
@@ -4,7 +4,7 @@ var MainPage = React.createClass({
     var title = '<h3>GymPal</h3>';
     return (
       <div className="row">
-        <div className="large-3 medium-4 columns">
+        <div className="large-2 medium-3 columns">
           <h5>Navigation</h5>
           <nav>
             <ul className="side-nav">
@@ -12,13 +12,96 @@ var MainPage = React.createClass({
             </ul>
           </nav>
         </div>
-        <div className="large-9 medium-8 columns">
+        <div className="large-10 medium-9 columns">
           <h1>GymPal - Your fitness companion!</h1>
+          <RoutineForm />
         </div>
       </div>
     );
   }
 });
+
+var RoutineForm = React.createClass({
+  getInitialState: function() {
+    return {
+      name: "",
+      additionalInfo: "",
+      duration: "",
+      toughness: "",
+      exerciseList: ""
+    };
+  },
+
+  render: function() {
+    return(
+      <form>
+        <div className="row">
+          <div className="large-12 medium-12 columns">
+            <RoutineInfo
+              nameChangeHandler = {this.handleNameChange}
+              additionalInfoChangeHandler = {this.handleAdditionalInfoChange}
+              durationChangeHandler = {this.handleDurationChange}
+              toughnessChangeHandler = {this.handleToughnessChange}
+              />
+            <Exercises />
+          </div>
+        </div>
+      </form>
+    );
+  },
+  handleNameChange: function(event) {
+    this.setState({name: event.target.value});
+  },
+  handleAdditionalInfoChange: function(event) {
+    this.setState({additionalInfo: event.target.value});
+  },
+  handleDurationChange: function(event) {
+    this.setState({duration: event.target.value});
+  },
+  handleToughnessChange: function(event) {
+    this.setState({toughness: event.target.value});
+  },
+});
+
+var RoutineInfo = React.createClass({
+  render: function() {
+    return(
+      <div className="row">
+        <div className="large-12 medium-12 columns">
+          <div className="row">
+            <label htmlFor="name">Routine name</label>&nbsp;
+            <input id="name" type="text" onChange={this.props.nameChangeHandler} />
+          </div>
+          <div className="row">
+            <label htmlFor="toughness">Toughness (0-100)</label>&nbsp;
+            <input id="toughness" type="number" min="0" max="100" onChange={this.props.toughnessChangeHandler} />
+          </div>
+          <div className="row">
+            <label htmlFor="duration">Duration (minutes)</label>&nbsp;
+            <input id="duration" type="number" min="0" max="1000" onChange={this.props.durationChangeHandler} />
+          </div>
+          <div className="row">
+            <label htmlFor="additionalInfo">Additional info</label>&nbsp;
+            <textarea id="additionalInfo" placeholder="More information about routine" rows="4" cols="80" onChange={this.props.additionalInfoChangeHandler}></textarea>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+var Exercises = React.createClass({
+  render: function() {
+    return(
+      <div className="row">
+        <div className="large-12 medium-12 columns">
+          <h3>Exercises</h3>
+        </div>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
   <MainPage />,
   document.getElementById('container')
